0ea8d6704f38f3cc446e991bc913358b45815ff5
Adrian Artacho <adrian.artacho@gmail.com>
2022-02-11 21:23:44 +0100

    Deleted all Subtrees

--- Tesser_INScore/Tesser_INScore/code/CurrentClip.js
+++
@@ -1,7 +0,0 @@
-// Get Playing Clip
-function getPlayingClip() {
-// function bang() {
-	var selectedTrack = new LiveAPI("live_set view selected_track");
-	var playingClipIndex = selectedTrack.get("playing_slot_index");
-	outlet(0, playingClipIndex);
-}
